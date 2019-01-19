1c470337f93c82ba86c7dcd39599b8b077ed1e45
Michał Adamski <michal@ert.pl>
2019-01-19 12:59:13 +0100

    first release

---
+++ action.js
@@ -0,0 +1,11 @@
+var documentClone = document.cloneNode(true);
+var article = new Readability(documentClone).parse();
+article.images = {};
+var counter = 1;
+for (let img of article.content.querySelectorAll('img')) {
+    let key = `img${("000000" + counter++).slice(-6)}`;
+    article.images[key] = img.src;
+    img.src = key;
+}
+article.content = article.content.innerHTML;
+article;
---
+++ background.js
@@ -0,0 +1,80 @@
+const gateways = [
+    'ipfs.io',
+    'hardbin.com',
+    'ipfs.eternum.io',
+    'cloudflare-ipfs.com',
+    'siderus.io',
+    'ipfs.infura.io'
+];
+
+function renderPage(title, content) {
+    return `<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/ipfs/QmSMwjABWVBsnS3Gha3xmjb5zKYndvFrS74iCAwykQCqcY/typesettings-1.2-min.css">
+        <title>${title}</title>
+        <style>
+            footer { padding: 3em 0; text-align: center; font-size: 1.4em; border-top: 1px solid #e1e1e1;}
+            a:hover { color: #222; border-bottom-color: #555; }
+            @media only screen and (min-width: 1441px) { .typesettings { font-size: 2.4em; } }
+            .typesettings { max-width: 33em; margin: 0 auto; }
+            a { color: #aeaeae; border-bottom: 1px dotted #aeaeae; text-decoration: none; }
+        </style>
+    </head>
+    <body>
+        <article class="typesettings golden">
+            <h1 style="margin-bottom: 1em">${title}</h1>
+            ${content}
+        </article>
+        <footer><a href="https://2read.tk/">2read.tk</a></footer>
+    </body>
+</html>`;
+}
+
+function ipfsPUT(hash, data, filename) {
+    return new Promise((resolve, reject) => {
+        let req = new XMLHttpRequest();
+        req.onreadystatechange = () => {
+            if (req.readyState != XMLHttpRequest.DONE) return;
+            if (req.status < 200 || req.status >= 300) {
+                return reject(`Unexpected status ${req.statusText} - ${req.status}`);
+            }
+            let hash = req.getResponseHeader("Ipfs-Hash");
+            resolve(hash);
+        };
+        req.onerror = reject;
+        req.open('PUT', `https://ipfs.eternum.io/ipfs/${hash}/${filename}`, true);
+        req.setRequestHeader('Content-Type', 'text/html');
+        req.send(data);
+    });
+}
+
+function seed(hash) {
+    for (let gateway of gateways) {
+        var req = new XMLHttpRequest();
+        req.open('GET', `https://${gateway}/ipfs/${hash}/`);
+        req.send();
+    }
+}
+
+async function handleClick() {
+    await browser.tabs.executeScript({ file: 'Readability.js' });
+    var result = await browser.tabs.executeScript({ file: 'action.js' });
+    let article = result[0];
+    // hash of empty folder
+    let hash = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn';
+    hash = await ipfsPUT(hash, renderPage(article.title, article.content), 'index.html');
+    for (let img in article.images) {
+        var response = await fetch(article.images[img], { mode: 'no-cors' });
+        if (response.status < 200 || response.status >= 400) {
+            console.error(`${response.status} - ${response.statusText}`);
+            continue;
+        }
+        hash = await ipfsPUT(hash, await response.blob(), img);
+    }
+    seed(hash);
+    browser.tabs.create({ url: `https://${gateways[0]}/ipfs/${hash}/` });
+}
+
+browser.browserAction.onClicked.addListener(handleClick);
